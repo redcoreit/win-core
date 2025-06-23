65a3c200d88caf65e0fd8629f553f40a3539ea58
Roland Halbaksz <roland.halbaksz@semilab.hu>
2025-06-23 08:52:42 +0200

    Add GH support for addpat64, update .vsconfig.

--- addpat64.ps1
+++ addpat64.ps1
@@ -1,3 +1,14 @@
+[CmdletBinding(DefaultParametersetName="setgh")]                  
+Param(
+    [parameter(ParameterSetName="setgh", Mandatory=$true)]
+    [switch]
+    $Gh,
+
+    [parameter(ParameterSetName="setaz", Mandatory=$true)]
+    [switch]
+    $Az 
+)
+
 $IncludePath = "$HOME\.gitpat64"
 
 # Ensure include path in .gitconfig file
@@ -11,7 +22,13 @@ $SecurePAT = Read-Host -Prompt "PAT" -AsSecureString
 
 # Convert SecureString to Base64 encoded string
 $PAT64 = $SecurePAT | ConvertFrom-SecureString -AsPlainText
-$PAT64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($PAT64))
+
+if ($Gh) {
+    $PAT64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes($PAT64))
+}
+elseif ($Az) {
+    $PAT64 = [Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes(":" + $PAT64))
+}
 
 # Write to ~/.gitpat64
 Add-Content -Path $IncludePath -Value "`n[http `"$URL`"]`n    extraHeader = `"Authorization: Basic $PAT64`""
